ba377210d6a02b81009a821d265da35dfbee5c76
robbie <robbielcampbell@hotmail.com>
2022-09-10 18:12:09 +0100

    gets images of all comments now too

--- Reddit/reddit_image_handler.py
+++ Reddit/reddit_image_handler.py
@@ -34,32 +34,40 @@ def store_web_image(url: str) -> None:
     title = wait_for_element(driver=driver,
                              xpath='/html/body/div[1]/div/div[2]/div[2]/div/div/div/div[2]/div[3]/div[1]/div[2]/div[1]')
 
-    set_window_focus(driver, title, "test.png")
+    set_window_focus(driver, title, "./video_images/title.png")
+
+    for main_comment in range(0, 3):
+        comment = wait_for_element(driver=driver,
+                                   xpath='//div[@style="padding-left:16px"]',
+                                   multiple=True)[main_comment]
+
+        set_window_focus(driver, comment, f"./video_images/{main_comment}.png")
+
     driver.quit()
 
 
-def wait_for_element(driver: webdriver, xpath: str, click=False) -> WebElement:
+def wait_for_element(driver: webdriver, xpath: str, click=False, multiple=False):
     try:
-        element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, xpath)))
-
+        element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, xpath))) if not multiple \
+            else WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, xpath)))
         return element if not click else element.click()
 
     except NoSuchElementException and TimeoutException:
         print(f"Could not find an element with the XPATH: {xpath}.")
-
         return None
 
 
 def set_window_focus(driver: webdriver, element: WebElement, path: str) -> None:
     x, y = element.location.values()
     height, width = element.size.values()
+    driver.execute_script(f"window.scrollTo(0, {y - 49})")  # 49px accounts for the navigation bar in reddit
     driver.save_screenshot(path)
     crop_focused_area(x, y, width, height, path)
 
 
 def crop_focused_area(x, y, width, height, path):
     image = Image.open(path)
-    crop_rectangle = (x, y, width + x, height + y)
+    crop_rectangle = (x, 49, width + x, height + 49)
     cropped_image = image.crop(crop_rectangle)
     cropped_image.save(path)
 
