e8954271dd10778d1ac4ae1e7c9421c37dc5713f
robbie <robbielcampbell@hotmail.com>
2022-12-19 11:54:55 +0000

    probably MVP for video to upload

--- VideoGeneration/video_generator.py
+++ VideoGeneration/video_generator.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from datetime import date
 from typing import List
 from moviepy.editor import *
+from moviepy.video.fx.all import crop
 from MediaGeneration.TTS.pyttsx3 import get_audio_file_duration
 from definitions import MEDIA_URL, ROOT_DIR
 from logger import Logger
@@ -28,7 +29,7 @@ def split_video_clips_into_mp3_sized_chunks(image_audio_pair: List[ImageAudioPai
     total_length: int = 0
     video_clips = []
     for index, pair in enumerate(image_audio_pair):
-        Logger.info(f"Appending {index}/{len(image_audio_pair) - 1} clips...")
+        Logger.info(f"Appending {index + 1}/{len(image_audio_pair)} clips...")
         audio_clip_length = get_audio_file_duration(pair.audio)
         audio_clip = AudioFileClip(pair.audio)
 
@@ -40,7 +41,7 @@ def split_video_clips_into_mp3_sized_chunks(image_audio_pair: List[ImageAudioPai
 
         total_length += audio_clip_length
 
-        written_index_value = str(index) + {1: 'st', 2: 'nd', 3: 'rd'}.get(4 if 10 <= index % 100 < 20 else index % 10, "th")
+        written_index_value = str(index + 1) + {1: 'st', 2: 'nd', 3: 'rd'}.get(4 if 10 <= index + 1 % 100 < 20 else index + 1 % 10, "th")
         Logger.info(f"Successfully appended {written_index_value} file.")
 
     return video_clips
@@ -52,22 +53,24 @@ def generate_youtube_video():
     """
     Logger.info(f"Entering {generate_youtube_video.__name__}")
 
-    clip = VideoFileClip(f"{MEDIA_URL}\\Videos\\video.mp4", audio=False)
-    clip = clip.resize((1080, 1920))
+    clip: VideoFileClip = VideoFileClip(f"{MEDIA_URL}\\Videos\\video.mp4", audio=False)
 
-    image_audio_pairs = [
+    Logger.info(f"Resizing the main clip...")
+
+    (w, h) = clip.size
+    cropped_clip = crop(clip, width=400, height=720, x_center=w/2, y_center=h/2)
+    resized_clip = cropped_clip.resize((1080, 1920))
+
+    Logger.info(f"Clip successfully resized.")
+
+    image_audio_pairs: List[ImageAudioPair] = [
         ImageAudioPair(image=f"{MEDIA_URL}\\Images\\title.png",
-                       audio=f"{MEDIA_URL}\\MP3s\\title.wav"),
-        ImageAudioPair(image=f"{MEDIA_URL}\\Images\\0.png",
-                       audio=f"{MEDIA_URL}\\MP3s\\0.wav"),
-        ImageAudioPair(image=f"{MEDIA_URL}\\Images\\1.png",
-                       audio=f"{MEDIA_URL}\\MP3s\\1.wav"),
-        ImageAudioPair(image=f"{MEDIA_URL}\\Images\\2.png",
-                       audio=f"{MEDIA_URL}\\MP3s\\2.wav"),
+                       audio=f"{MEDIA_URL}\\MP3s\\title.wav")
     ]
 
+    image_audio_pairs.extend([ImageAudioPair(image=f"{MEDIA_URL}\\Images\\{x}.png", audio=f"{MEDIA_URL}\\MP3s\\{x}.wav") for x in range(0, 2)])
 
-    video_clip_list = split_video_clips_into_mp3_sized_chunks(image_audio_pairs, clip)
+    video_clip_list = split_video_clips_into_mp3_sized_chunks(image_audio_pairs, resized_clip)
 
     Logger.info(f"Attempting to concatenate videos...")
 
--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from MediaGeneration.generate_media_files import generate_and_save_media_files
 from VideoGeneration.video_generator import generate_youtube_video
 
 if __name__ == "__main__":
-    generate_and_save_media_files("askreddit")
+    # generate_and_save_media_files("askreddit")
     generate_youtube_video()
